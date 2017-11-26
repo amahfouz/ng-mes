f1ac76f31e5cf6e30aa87889b9e43f7806aa5534
Ayman Mahfouz <amahfouz@webalo.com>
2017-11-25 21:05:35 -0800

    src/app/time-scale/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { TimeLineComponent } from './time-line/time-line.component';
+import { TimeScaleService } from './time-scale/time-scale.service';
+import { PeriodComponent } from './period/period.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TimeLineComponent,
+    PeriodComponent
+],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TimeScaleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
